97eb8a955debb4fee69711d41fb31abc5b946732
Katrina Li <kl502@cam.ac.uk>
2023-04-01 01:13:40 +0100

    extract_acoustics.py incorporate f0 data extraction

--- extract_acoustics.py
+++ extract_acoustics.py
@@ -3,6 +3,9 @@ import textgrid
 import pandas as pd
 import re
 import os
+import parselmouth
+from parselmouth.praat import call
+import math
 # Read the excel
 Template = pd.read_excel("Template.xlsx",header=0,sheet_name="Cantonese")
 Template = Template.drop("Original",axis=1)
@@ -24,11 +27,14 @@ with open("Directory.txt") as f:
         pathdict[language] = path
 current_directory = pathdict["Cantonese"]
 directory_textgrid = current_directory + "/textgrid_pitch_batch/**/*.TextGrid"
+directory_sound = current_directory + "/sound_original/"
+output_tsv = open("./results/Can_data.tsv","w")
 
 # Play with the actual annotation dataframe
 textgrid_cantonese = glob.glob(directory_textgrid,recursive = True)
 fulldata_char = pd.DataFrame()
 for file in textgrid_cantonese:
+    ## Propcess the Textgrid for duration information
     tg = textgrid.TextGrid.fromFile(file)
     char_tier_manual = tg[4]
     char_manual = [char_tier_manual[i].mark for i in range(0, len(char_tier_manual))]
@@ -107,14 +113,93 @@ for file in textgrid_cantonese:
                     sen_index.append(0)
                     note.append("Other final stuff")
                     p +=1
-    # Check if the length are equal
+    # Check if the length of list is equal
     assert len(char_manual) == len(mintime_char_manual) == len(maxtime_char_manual) == len(sen_index) == len(case)
+    # Check where the errors are from
     # if len(char_manual) != len(sen_index):
         # print("Unequal:", textgridname, "\n", char_manual, "\n", template_char,"\n", sen_index)
-    data_char_manual = pd.DataFrame([sen_index, char_manual, case, mintime_char_manual, maxtime_char_manual])
-    data_char_manual = data_char_manual.transpose()
-    data_char_manual.columns = ["idx","character","case", "minTime","maxTime"]
+    
+    ## Incorporate the f0 infomation
     textgridname = file.split("/")[-1]
-    data_char_manual['name'] = textgridname
-    fulldata_char = pd.concat([fulldata_char,data_char_manual])
-fulldata_char.to_csv("./results/Cantonese_char_dur_withcase.csv", index = False)
+    soundname = directory_sound + textgridname.split("_")[0] + ".wav"
+    sound = parselmouth.Sound(soundname)
+    f0_tier = tg[6]
+    # The number of points extracted from each interval
+    number_of_points = 10
+    ## Calculate the best f0 range based on Hirst (2001)
+    # The first pass
+    pitch1 = call(sound, "To Pitch", 0.0, 50, 800)
+    min1 = call(pitch1, "Get minimum", 0, 0, "Hertz", "None")
+    max1 = call(pitch1, "Get maximum", 0, 0, "Hertz", "None")
+    q1 = call(pitch1, "Get quantile", 0, 0, 0.25, "Hertz")
+    q3 = call(pitch1, "Get quantile", 0, 0, 0.75, "Hertz")
+    q1 = math.floor(q1)
+    q3 = math.ceil(q3)
+    # The second pass
+    defaultf0floor = math.floor((0.7 * q1)/ 10) * 10
+    defaultf0ceiling = math.ceil((2.5 * q3)/ 10) * 10
+    pitch2 = call(sound, "To Pitch", 0.0, defaultf0floor, defaultf0ceiling)
+
+    rhyme_info_series = []
+    sen_index_rhyme = []
+    for i in range(0, len(f0_tier)):
+        rhyme_label = f0_tier[i].mark
+        if rhyme_label != "":
+            start = f0_tier[i].minTime
+            end = f0_tier[i].maxTime
+            # Get normtime f0
+            duration = end-start
+            f0 = []
+            for x in range(0,number_of_points):
+                normtime = start + duration*(x-1)/(number_of_points)
+                f0_at_normtime = call(pitch2, "Get value at time", normtime, 'Hertz', 'Linear')
+                f0_at_normtime_formatted = float("{:.3f}".format(f0_at_normtime))
+                f0.append(f0_at_normtime_formatted)
+            assert len(f0) == number_of_points
+            # print(label, "\t", start, "\t",end, "\t",f0)
+            # Get f0 statistics
+            f0min = call(pitch2, "Get minimum", start, end, "Hertz", "None")
+            f0min_formatted = float("{:.3f}".format(f0min))
+            f0max = call(pitch2, "Get maximum", start, end, "Hertz", "None")
+            f0max_formatted = float("{:.3f}".format(f0max))
+            f0min_time = call(pitch2, "Get time of minimum", start, end, "Hertz", "Parabolic")
+            f0max_time = call(pitch2, "Get time of maximum", start, end, "Hertz", "Parabolic")
+            #print(label, "\t", f0min_time, "\t",f0max_time)
+            ## Compare the number stamp, to map the rhyme tier to the processed character tier
+            # Consider mapping to the already built character tier
+            mid = (start+end)/2
+            # Compare
+            index = 0
+            # the last interval causes problem; filter it first
+            while index < len(mintime_char_manual) and mid > mintime_char_manual[index]:
+                index += 1
+            # The actual index is actually one step back
+            index = index-1
+            # check the tiem stamp indeed in between two values
+            assert mid < maxtime_char_manual[index]
+            # !This is the index in the original list, that can be used to map with the previous list!
+            sen_index_rhyme.append(Sen_index[index])
+            rhyme_info = [str(Sen_index[index]), rhyme_label, str(duration), str(f0min_formatted), str(f0max_formatted),str(f0min_time),str(f0max_time)]
+            f0_string = map(str, f0)
+            rhyme_info.extend(f0_string)
+            rhyme_info_series.append(rhyme_info)
+            # rhyme_info.extend()
+            # print(rhyme_info)
+    # print(rhyme_info_series)
+    # Write lines to file
+    for i in range(0,len(char_manual)):
+        out = []
+        out.extend([str(sen_index[i]), char_manual[i], case[i], str(mintime_char_manual[i]), str(maxtime_char_manual[i])])
+        if sen_index[i] in sen_index_rhyme:
+            rhyme_index = sen_index_rhyme.index(sen_index[i])
+            out.extend(rhyme_info_series[rhyme_index])
+            # print(out)
+        output_tsv.write("\t".join(out) + "\n")
+
+    # Write out the information
+    # data_char_manual = pd.DataFrame([sen_index, char_manual, case, mintime_char_manual, maxtime_char_manual])
+    # data_char_manual = data_char_manual.transpose()
+    # data_char_manual.columns = ["idx","character","case", "minTime","maxTime"]
+    # data_char_manual['name'] = textgridname
+    # fulldata_char = pd.concat([fulldata_char,data_char_manual])
+# fulldata_char.to_csv("./results/Cantonese_char_dur_withcase.csv", index = False)
